3b66daa2139c06524a22d1ce75a02b039ad63032
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-07-15 18:12:54 -0400

    adding service worker

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "./public/index.html",
+  "./public/css/style.css",
+  "./public/js/idb.js",
+  "./public/js/index.js",
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
