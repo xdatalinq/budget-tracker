a27f393943740f1b23ab13e514a196571f32d4bf
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-07-15 22:20:16 -0400

    Deploying

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
