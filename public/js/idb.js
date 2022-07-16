a24e6c751649f1c8bf457c9af87220032c2206d9
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-07-15 21:24:15 -0400

    service worker/idb.js fix

--- public/js/idb.js
+++ public/js/idb.js
@@ -53,7 +53,7 @@ function uploadBudget() {
   getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch("/api/transactions", {
+      fetch("/api/transaction", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
--- public/service-worker.js
+++ public/service-worker.js
@@ -17,6 +17,7 @@ self.addEventListener("install", function (e) {
     })
   );
 });
+self.skipWaiting();
 
 self.addEventListener("activate", function (e) {
   e.waitUntil(
